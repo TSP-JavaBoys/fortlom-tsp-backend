bf71360983991270d46897b33d58e80f103b8cea
giusencito <66499088+giusencito@users.noreply.github.com>
2023-08-31 18:27:15 -0500

    Feat: add test configuration

---
+++ src/test/java/com/example/fortlomtsp/StepDefinitions/PublicationsOfEventsSteps.java
@@ -0,0 +1,6 @@
+package com.example.fortlomtsp.StepDefinitions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class PublicationsOfEventsSteps {
+    WebDriver driver = null;
+}
