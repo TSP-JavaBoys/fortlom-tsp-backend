238561364e98787a3982084537d66925a16b3801
Angie_Naye_Ruiz <89089445+Angie-RC@users.noreply.github.com>
2023-10-27 00:52:25 -0500

    update

--- src/main/java/com/example/fortlomtsp/backend/api/AlbumController.java
+++ src/main/java/com/example/fortlomtsp/backend/api/AlbumController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/api/v1/userservice/albums")
 public class AlbumController {
     @Autowired
--- src/main/java/com/example/fortlomtsp/backend/api/SongController.java
+++ src/main/java/com/example/fortlomtsp/backend/api/SongController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/api/v1/userservice/albums/songs")
 public class SongController {
     @Autowired
